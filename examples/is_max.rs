aa0ff799ab895b3ef1ee76c9921fa12eb43853d9
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-10-21 13:18:01 -0700

    Print out player info in is_max info

--- examples/is_max.rs
+++ examples/is_max.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello world!");
+extern crate rustrunemetrics;
+use rustrunemetrics::client;
+
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about = "Interact with the RuneMetrics API.", long_about = None)]
+struct Args {
+    /// Name of the player whose stats you want to query
+    #[arg(short, long)]
+    player_name: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let player_name = args.player_name;
+    let player_profile = client::get_profile(player_name)?;
+
+    println!("{:?}", player_profile);
+
+    Ok(())
 }
