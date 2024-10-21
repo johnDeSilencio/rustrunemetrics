78da8065ab2905fad845b052ad627c96fb6f5657
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-10-21 13:16:04 -0700

    Add custom parsers for rank, loggedIn, and date fields

--- src/types.rs
+++ src/types.rs
@@ -5,6 +5,7 @@ use std::str::FromStr;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Activity {
+    #[serde(deserialize_with = "parse_date_time")]
     pub date: DateTime<Utc>,
     pub details: String,
     pub text: String,
@@ -14,7 +15,9 @@ pub struct Activity {
 pub struct SkillValue {
     pub id: Skill,
     pub level: u32,
-    pub rank: u32,
+
+    // #[serde(deserialize_with = "parse_rank")]
+    pub rank: Option<u32>,
     pub xp: u32,
 }
 
@@ -25,7 +28,7 @@ pub struct PlayerProfile {
     #[serde(rename = "combatlevel")]
     pub combat_level: u32,
 
-    #[serde(rename = "loggedIn")]
+    #[serde(rename = "loggedIn", deserialize_with = "parse_string_bool")]
     pub logged_in: bool,
 
     #[serde(rename = "magic")]
@@ -46,7 +49,9 @@ pub struct PlayerProfile {
 
     #[serde(rename = "ranged")]
     pub ranged_xp: u32,
-    pub rank: u32,
+
+    #[serde(deserialize_with = "parse_rank")]
+    pub rank: Option<u32>,
 
     #[serde(rename = "skillvalues")]
     pub skill_values: Vec<SkillValue>,
@@ -111,4 +116,54 @@ pub enum QuestStatus {
     Completed,
     Started,
     NotStarted,
+}
+
+fn parse_rank<'de, D>(deserializer: D) -> Result<Option<u32>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let raw_rank: Option<String> = Option::deserialize(deserializer)?;
+
+    let Some(raw_rank) = raw_rank else {
+        return Ok(None);
+    };
+
+    // The rank is returned as a string rather than a number,
+    // so we have to remove the commas and convert to a u32.
+    //
+    // For example, "123,456" becomes 123456u32.
+    let raw_rank = raw_rank.as_str().replace(",", "");
+
+    let Ok(rank) = str::parse(raw_rank.as_str()) else {
+        return Ok(None);
+    };
+
+    Ok(Some(rank))
+}
+
+fn parse_string_bool<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    use serde::de::Error;
+
+    // The metrics server encodes these bools in a string, for some strange reason
+    let raw_bool: String = Deserialize::deserialize(deserializer)?;
+    let parsed_bool = FromStr::from_str(raw_bool.as_str()).map_err(Error::custom)?;
+
+    Ok(parsed_bool)
+}
+
+fn parse_date_time<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    use serde::de::Error;
+
+    let raw_date_time: String = Deserialize::deserialize(deserializer)?;
+    let parsed_date_time = NaiveDateTime::parse_from_str(raw_date_time.as_str(), "%d-%b-%Y %H:%M")
+        .map_err(Error::custom)?
+        .and_utc();
+
+    Ok(parsed_date_time)
 }
